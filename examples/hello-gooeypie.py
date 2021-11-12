2e001ad335c49a152bd5a3dd2d6e5f3c3fd11da6
Adam Antonio <adamantonio@gmail.com>
2021-11-12 18:12:28 +1100

    Refactor hello-gooeypie.py
    Some small changes to variable naming and overall order of code to be more inline with other examples on the website

--- examples/hello-gooeypie.py
+++ examples/hello-gooeypie.py
@@ -1,16 +1,16 @@
 import gooeypie as gp
 
 def say_hello(event):
-    # hello_label.text = 'Hello Gooey Pie!'
-    pass
+    hello_lbl.text = 'Hello Gooey Pie!'
+
 app = gp.GooeyPieApp('Hello!')
-app.set_size(250, 80)
+app.width = 250
+
+hello_btn = gp.Button(app, 'Say Hello', say_hello)
+hello_lbl = gp.Label(app, '')
+
 app.set_grid(2, 1)
-#
-hello_button = gp.Button(app, 'Say Hello', say_hello)
-hello_label = gp.Label(app, '')
-#
-app.add(hello_button, 1, 1, align='center')
-app.add(hello_label, 2, 1, align='center')
+app.add(hello_btn, 1, 1, align='center')
+app.add(hello_lbl, 2, 1, align='center')
 
 app.run()
